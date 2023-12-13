050edd71a339e36c8f918b2c376bdbb7e73cb000
davidisnardi <davidisnardi38@gmail.com>
2023-12-13 15:33:39 -0300

    Clase2

--- ProductManager.js
+++ ProductManager.js
@@ -1,82 +1,73 @@
-const fs = require("node:fs");
-
 class ProductManager {
-    constructor() {
-     this.products = []
-   };
-
-   addProduct(title, description, price, thumbnail, code, stock){
-    if (this.ValidateField(title, description, price, thumbnail, code, stock) && this.DuplicateCode(code)){
-     const newProduct = {
-      id: this.IdGenerator(),
-      title: title,
-      description: description,
-      price: price,
-      thumbnail: thumbnail,
-      code: code,
-      stock: stock
-      };
-
-     this.products.push(newProduct)
-     console.log(this.products)
+  constructor() {
+   this.products = []
+ };
+ 
+ addProduct(title, description, price, thumbnail, code, stock){
+  if (this.ValidateField(title, description, price, thumbnail, code, stock) && this.DuplicateCode(code)){
+   const nuevoProducto = {
+    id: this.IdGenerator(),
+    title: title,
+    description: description,
+    price: price,
+    thumbnail: thumbnail,
+    code: code,
+    stock: stock
     };
-   };
-
-   getProducts() {
-    return this.products
-   };
-
-   getProductsById(Productid){
-    const result = this.products.find((product) => product.id === Productid);
-    if(!result) {
-     return console.log("Not found")
-    }
-    return result
-   };
-
-   IdGenerator() {
-     let id = 0;
-      if (this.products.length === 0) {
-       id = 1; 
-
-     } else {
-       id = this.products[this.products.length - 1].id + 1;
-     };
-
-   return id;
-   };
-
-   DuplicateCode(code){
-     if (this.products.find((product) => product.code === code) !== undefined) {
-     console.error("Este producto ya existe")
-     return false;
-
+ 
+   this.products.push(nuevoProducto)
+   console.log(this.products)
+  };
+ };
+ 
+ getProducts() {
+  return this.products
+ };
+ 
+ getProductsById(id){
+  return this.products.find((product) => product.id === id);
+ };
+ 
+ IdGenerator() {
+   let id = 0;
+    if (this.products.length === 0) {
+     id = 1; 
+ 
    } else {
-     return true;
+     id = this.products[this.products.length - 1].id + 1;
    };
-   };
-
-
-   ValidateField(title, description, price, thumbnail, code, stock) {
-   if (!title || !description || !price || !thumbnail || !code || !stock) {
-     console.error("Por favor, completa los campos");
-     return false;
-
-   } else {
-     return true;
-   };
-   };
-
-
-   };
-
-   const newProductManager = new ProductManager();
-
-   newProductManager.addProduct("Pan", "Bimbo", 300, "SinImagen", "123", 10);
-   newProductManager.addProduct("Queso", "Calcar", 100, "SinImagen", "124", 5);
-   //Codigo Duplicado
-   newProductManager.addProduct("Queso", "Calcar", 100, "SinImagen", "124", 5);
-   //Campo sin completar
-   newProductManager.addProduct("Pan", 300, "123", 10);
-
-   console.log(newProductManager.getProductsById(1)) 
+ 
+ return id;
+ };
+ 
+ DuplicateCode(code){
+   if (this.products.find((product) => product.code === code) !== undefined) {
+   console.error("Este producto ya existe")
+   return false;
+ 
+ } else {
+   return true;
+ };
+ };
+ 
+ ValidateField(title, description, price, thumbnail, code, stock) {
+ if (!title || !description || !price || !thumbnail || !code || !stock) {
+   console.error("Por favor, completa los campos");
+   return false;
+ 
+ } else {
+   return true;
+ };
+ };
+ 
+ };
+ 
+ const newProductManager = new ProductManager();
+ 
+ newProductManager.addProduct("Pan", "Bimbo", 300, "SinImagen", "123", 10);
+ newProductManager.addProduct("Queso", "Calcar", 100, "SinImagen", "124", 5);
+ //Codigo Duplicado
+ newProductManager.addProduct("Queso", "Calcar", 100, "SinImagen", "124", 5);
+ //Campo sin completar
+ newProductManager.addProduct("Pan", 300, "123", 10);
+ 
